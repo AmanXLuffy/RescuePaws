5598696cc7d05bd629660c18a89ef4919e252b75
AMAN <amansolanki0702@gmail.com>
2025-06-14 12:11:13 +0530

    Fix backend start script for Render

--- src/components/activity.jsx
+++ src/components/activity.jsx
@@ -1,38 +1,3 @@
-// import React from 'react'
-// import Navbar from "./navbar";
-// import Footer from "./footer";
-// import Bear from "../animation/bear";
-// import Hamster from "../animation/hamster"
-// import { SignedIn, SignedOut, SignInButton } from '@clerk/clerk-react';
-
-// function Activity() {
-//   return (
-//     <>
-//       <Navbar />
-
-//       <SignedOut>
-//         <div className="w-full h-[600px] flex items-center justify-center bg-white text-center">
-//           <div className='flex flex-col w-[900px] h-[500px] items-center justify-center bg-gray-300 border-2 border-gray-300 rounded-lg p-8'>
-//             <div className="loader"></div> 
-
-//           <p className="text-2xl sm:text-3xl font-semibold mb-6 text-gray-800"> YOU NEED TO SIGN IN DUDE </p>
-
-//           <button className="px-6 py-2 border-2 border-black rounded text-lg hover:bg-black hover:text-white transition-all">
-//             <SignInButton />
-//           </button>
-
-//         </div>
-//           </div>
-//       </SignedOut>
-
-//       <Footer />
-//     </>
-//   );
-// }
-
-// export default Activity;
-
-
 import React from 'react';
 import Navbar from "./navbar";
 import Footer from "./footer";
